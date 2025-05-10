c6177c2ca1abf340296d65dc1f141212823698d1
Adnan FAIZE <adnanfaize@gmail.com>
2025-05-10 20:30:15 +0200

    préparation pour un composant de visualisation des données : heatmap

---
+++ frontend/src/routes/_components/data_visualizers/HeatMap.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let { showModal = $bindable() } = $props();
+
+  import PopupWindow from "../PopupWindow.svelte";
+</script>
+
+<PopupWindow bind:showModal>
+  {#snippet header()}
+    <h2>a</h2>
+  {/snippet}
+  <p>a</p>
+</PopupWindow>
