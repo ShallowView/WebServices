c394d325f5beebf05f3fec6d6f2b23c0c2a95895
Xibitol <xibitol@pimous.dev>
2025-05-16 22:46:01 +0200

    Completed web deployment and added static master website.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,18 +6,19 @@ source "bin/dockerUtils.sh"
 source "bin/config.sh"
 # ---
 mode=${1-"prod"}
-resourceDir="bin/docker"
 
 # ---
+# BUG: Build will wait for changes because of paraglideVitePlugin
+#npm run -w frontend build
+
 stop $DOCKER_CONTAINER_NAME
 
 removeImage $DOCKER_IMAGE_REFERENCE
-buildImage $DOCKER_IMAGE_REFERENCE "$resourceDir/$DOCKER_FILE" "$resourceDir"
+buildImage $DOCKER_IMAGE_REFERENCE "bin/docker/$DOCKER_FILE" .
 
 createVolumes $DOCKER_CONFIG_VOLUME_NAME $DOCKER_DATA_VOLUME_NAME
 
 createContainer \
 	$DOCKER_CONTAINER_NAME $DOCKER_IMAGE_REFERENCE "$mode" \
 	-v $DOCKER_CONFIG_VOLUME_NAME:/config \
-	-v $DOCKER_DATA_VOLUME_NAME:/data \
-	-p $EXPOSED_PORT:80 \
+	-v $DOCKER_DATA_VOLUME_NAME:/data
